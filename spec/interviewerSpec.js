2910508c029ef0e2983bcc58c8a1e5f5a3238b32
Phips Peter <pspeter333@gmail.com>
2011-11-03 15:10:57 -0500

    Updated the specs

--- spec/interviewerSpec.js
+++ spec/interviewerSpec.js
@@ -1,15 +1,15 @@
 describe('Interviewer', function() {
   it('should be defined', function() {
-    expect(Interviewer).toBeDefined();
+    expect(window.Interviewer).toBeDefined();
   });
   
   it('should have a name', function() {
-    var interviewer = new Intervier({name: 'A+B'});
+    var interviewer = new window.Intervier({name: 'A+B'});
     expect(interviewer.get('name')).toEqual('A+B');
   });
   
   it('should have a cid', function() {
-    var interviewer = new Intervier({name: 'A+B'});
+    var interviewer = new window.Intervier({name: 'A+B'});
     expect(interviewer.cid).toBeDefined();
   })
 }); 
