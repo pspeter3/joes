0408725b024e2b09252d6884931085a257ce4038
Phips Peter <pspeter333@gmail.com>
2011-11-03 14:55:28 -0500

    added the interviewer spec

---
+++ spec/interviewerSpec.js
@@ -0,0 +1,15 @@
+describe('Interviewer', function() {
+  it('should be defined', function() {
+    expect(window.Interviewer).toBeDefined();
+  });
+  
+  it('should have a name', function() {
+    var interviewer = new Intervier({name: 'A+B'});
+    expect(interviewer.get('name')).toEqual('A+B');
+  });
+  
+  it('should have a cid', function() {
+    var interviewer = new Intervier({name: 'A+B'});
+    expect(interviewer.cid).toBeDefined();
+  })
+}); 
