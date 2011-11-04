2b14faf79a9d8705ff9e90c71bfecc8267a9f03e
Phips Peter <pspeter333@gmail.com>
2011-11-03 22:37:21 -0500

    fixed the interviewer

---
+++ js/interviewer.js
@@ -0,0 +1,23 @@
+// Create the constructor
+window.Interviewer = function(id, name) {
+  this.id = id;
+  this.name = name;
+}
+
+// Create a static hashmap to hold id values based on name
+window.Interviewer.hash = {};
+
+// Create a static id count
+window.Interviewer.idCount = 0;
+
+// Create a static creation function that prevents multiple entries
+window.Interviewer.create = function(name) {
+  if (_(window.Interviewer.hash[name]).isUndefined()) {
+    var interviewer = new window.Interviewer(
+      window.Interviewer.idCount++,
+      name
+    );
+    window.Interviewer.hash[name] = interviewer;
+  }
+  return window.Interviewer.hash[name];
+}
