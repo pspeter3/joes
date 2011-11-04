cb58817c646d2a0ae4b2db8025c15c575d2aadb3
Phips Peter <pspeter333@gmail.com>
2011-11-03 23:02:14 -0500

    Added specs to the recruit suite

--- js/recruit.js
+++ js/recruit.js
@@ -1,5 +1,14 @@
-function Recruit(fullname, date, pair) {
-  this.fullname_ = fullname;
-  this.date_ = date;
-  this.pair_ = pair;
+// Constructor
+window.Recruit = function(name, date, interviewerId) {
+  this.id = window.Recruit.idCount++;
+  this.name = name;
+  this.date = date;
+  this.interviewerId = interviewerId;
+  this.conflicts = {interviewerId: true};
+  this.temp = {};
+  this.fellows = [];
 }
+
+// Create an id count
+window.Recruit.idCount = 0;
+
---
+++ spec/recruitSpec.js
@@ -0,0 +1,26 @@
+describe('Recruit', function() {
+  it('should be defined', function() {
+    expect(window.Recruit).toBeDefined();
+  });
+  
+  it('should construct a simple object', function() {
+    var r = new window.Recruit('A', 'Saturday', 0);
+    expect(r.id).toEqual(0);
+    expect(r.name).toEqual('A');
+    expect(r.date).toEqual('Saturday');
+  });
+  
+  it('should handle various date inputs', function() {
+    var saturday = new window.Recruit('A', 'Saturday, Nov 18', 0);
+    expect(saturday.date).toEqual('Saturday');
+    var sunday = new window.Recruit('A', 'Nov 19, Sunday', 0);
+    expect(sunday.date).toEqual('Sunday');
+    var either = new window.Recruit('A', 'Gargbage', 0);
+    expect(either.date).toEqual('Either');
+  });
+  
+  it('should have a render method', function() {
+    var r = new window.Recruit('A', 'Saturday', 0);
+    expect(r.render).toBeDefined();
+  });
+});
