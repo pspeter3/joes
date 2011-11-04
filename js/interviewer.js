619f7d0f3e7c5edf82ab52f1a3be28bde4c9848b
Phips Peter <pspeter333@gmail.com>
2011-11-03 19:31:24 -0500

    Added an InterviewCollection object

--- js/interviewer.js
+++ js/interviewer.js
@@ -1,1 +1,8 @@
+// Create a default backbone model
 window.Interviewer = Backbone.Model.extend({});
+
+// Create the Interviewer Collection
+window.InterviewCollection = Backbone.Collection.extend({
+  model: window.Interviewer,
+  cache: {}
+});
