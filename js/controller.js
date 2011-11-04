673395d468c2ebb0a7d7a9e49d33cc287a62886e
Phips Peter <pspeter333@gmail.com>
2011-11-04 01:58:22 -0500

    managed to make event headers appear

--- js/controller.js
+++ js/controller.js
@@ -1,15 +1,25 @@
 $(function() {
   $("form").submit(function() {
-    console.log("hey");
+    // Parse the CSV data
     var data = window.parseCSV($('#recruits').val());
-    var recruits = [];
+    saturday = {};
+    sunday = {}
     _(data).each(function(row) {
       if (row.length === 3) {
         var interviewer = window.Interviewer.create(row[2]);
-        recruits.push(new window.Recruit(row[0], row[1], interviewer.id));
+        var recruit = new window.Recruit(row[0], row[1], interviewer.id);
+        if (recruit.date === 0) {
+          saturday[recruit.id] = recruit;
+        } else {
+          sunday[recruit.id] = recruit;
+        }
       }
     });
-    alert("hey");
+    // Add events to the tables
+    var numEvents = parseInt($('#events').val());
+    for (var i = 0; i < numEvents; i++) {
+      $('.events thead tr').append('<th>' + (i + 1) + '</th>');
+    }
     return false;
   });
 });
